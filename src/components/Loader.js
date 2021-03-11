8edafc29c0080098bcbf962406a377959d0a1586
Garbanv <garban.valdeon@gmail.com>
2021-03-11 12:41:38 +0100

    remove icons bi

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,11 @@
 import React from 'react'
-// import {RiLoader4Fill} from 'react-icons/Ri'
+import {RiLoader4Fill} from 'react-icons/Ri'
 
 export const Loader = ({text}) => {
     return (
         <div className="flex space-between loader text-sm">
-            {/* <RiLoader4Fill className="icon-spin mt-0.5 mr-1 "/> */}
-            <h3>Loading ...</h3>
+            <RiLoader4Fill className="icon-spin mt-0.5 mr-1 "/>
+
             {` ${text}`}
         </div>
     )
