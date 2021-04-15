b34c9404c7edb66d7f5bda33cdd9d58d29d07056
Garbanv <garban.valdeon@gmail.com>
2021-04-15 11:34:23 +0200

    npm update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,6 @@ module.exports = {
     author: `@platformable`,
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-load-script',
-      options: {
-        disable: !process.env.SENTRY_DSN, // When do you want to disable it ?
-        src: 'https://browser.sentry-cdn.com/5.15.4/bundle.min.js',
-        integrity:
-          'sha384-Nrg+xiw+qRl3grVrxJtWazjeZmUwoSt0FAVsbthlJ5OMpx0G08bqIq3b/v0hPjhB',
-        crossorigin: 'anonymous',
-        onLoad: `() => Sentry.init({dsn:"${process.env.SENTRY_DSN}"})`,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
